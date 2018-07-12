3671b1c6a517a317b3b70d25d3f8c15b0af7960b
Canivet Geoffrey <geoffreycanivet@gmail.com>
2018-07-12 22:25:31 +0100

    Variables

---
+++ 01.js
@@ -0,0 +1,25 @@
+// EXERCICE 1 :
+let metier = "codeur";
+let pays = "belgique";
+let prenom = "vous même";
+let enfants = 3;
+
+console.log("Vous serez "+metier+" et habiterez en "+pays+" et marié à "+prenom+" avec "+enfants+" enfants");
+
+// EXERCICE 2 :
+let anneeCourante = 2018;
+let anneePersonne = 1985;
+
+console.log(anneeCourante - anneePersonne);
+
+// EXERCICE 3 :
+let age = 33;
+let ageMax = 100;
+let danree = "conconbres";
+let danreeQuot = 2;
+let danreeAnnee = 365 * danreeQuot;
+let ageRestant = ageMax + age;
+let danreeFinal = danreeAnnee * ageRestant;
+
+console.log("il vous reste " +danreeFinal+" "+danree+" avant d'atteindre l'age de "+ageMax+" ans")
+
