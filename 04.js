8ae6753f9e9a79d028c396cb8648ed939b148992
Canivet Geoffrey <geoffreycanivet@gmail.com>
2018-07-13 12:15:54 +0100

    boucles

---
+++ 04.js
@@ -0,0 +1,15 @@
+//EXERCICES 1 : 
+for (let i = 0; i<=20; i++) {
+    if(i%2 == 0) {
+        console.log(i + " est pair");
+    } else {
+        console.log(i + " est impair");
+    }
+}
+
+// EXERCICES 2 :
+for (let i = 0; i<=10; i++) {
+    console.log(i +" * 9 = "+ i * 9);
+}
+
+// EXERCICES 3 : 
