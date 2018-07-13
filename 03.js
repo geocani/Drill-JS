1bcd0fa77a39c1c14af9040185790744ff9c5398
Canivet Geoffrey <geoffreycanivet@gmail.com>
2018-07-13 11:41:17 +0100

    fonctions

---
+++ 03.js
@@ -0,0 +1,24 @@
+let fSoustraction = function(a, b){
+    console.log(a - b);
+}
+fSoustraction(10, 8);
+
+let fDivision = function(a, b){
+    console.log(a / b);
+}
+fDivision(12, 3);
+
+let fMulti = function(a, b){
+    console.log(a * b);
+}
+fMulti(3, 3);
+
+let fPourCent = function(a, b){
+    console.log((a * b)/100);
+}
+fPourCent(100, 50);
+
+let fKmH = function(a, b){
+    return (a / b);
+}
+console.log("je fais " +fKmH(100, 4)+" km/h");
